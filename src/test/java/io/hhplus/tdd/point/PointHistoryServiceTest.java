0a30d9ae307f6fef5c58dbe7ebd1c3c81a20c5cd
crocusia <rainbowsubin0522@gmail.com>
2025-10-24 09:23:03 +0900

    [Docs] Step1 테스트 코드의 불필요한 import 제거

--- src/test/java/io/hhplus/tdd/point/PointHistoryServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointHistoryServiceTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 /**
